995212a0864d92ba3bb3f00a7bc2ddac0ac440df
Zeke Sikelianos <zeke@sikelianos.com>
2009-01-27 15:31:56 -0800

    Finished a first-round functional version of the GET param autopopulator

--- greasemonkey/universal_get_params_form_populater.user.js
+++ greasemonkey/universal_get_params_form_populater.user.js
@@ -1,29 +1,41 @@
 // ==UserScript==
-// @name           Delicious Tags From URL
+// @name           Universal GET Params Form Populator
 // @namespace      http://zeke.sikelianos.com
-// @description    This script finds text fields on the page and populates them with the values of an corresponding GET parameters
-// http://internetducttape.com/2008/05/08/greasemonkey-ninja-jquery/
+// @date           2009-01-27
+// @creator        Zeke Sikelianos (zeke.sikelianos.com), with jQuery inclusion help from http://joanpiedra.com/jquery/greasemonkey/
+// @description    This script finds text fields on the page and populates them with the values of any corresponding GET parameters
 // @include        *
 // ==/UserScript==
 
-var tab_box = document.getElementById('tags');
+// Add jQuery
+var GM_JQ = document.createElement('script')
+GM_JQ.src = 'http://jquery.com/src/jquery-latest.js'
+GM_JQ.type = 'text/javascript'
+document.getElementsByTagName('head')[0].appendChild(GM_JQ)
 
-if (tab_box != null){
-	urltag = gup('tags');
-	if (urltag != null){
-		tab_box.value = unescape(urltag);
+// Check if jQuery's loaded
+function GM_wait() {
+	if(typeof unsafeWindow.jQuery == 'undefined') { window.setTimeout(GM_wait,100); }
+	else { $ = unsafeWindow.jQuery; letsJQuery(); }
+}
+GM_wait()
+
+// Main function
+function letsJQuery() {
+	var inputs = $("form :text")
+	for (var i=0; i<inputs.length; i++) {
+		var input = inputs[i]
+		var get = value_of_get_param(input.name)
+		if (get != null) {
+			input.value = get
+		}
 	}
 }
 
-// Helper functions
-function gup( name )
-{
-  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-  var regexS = "[\\?&]"+name+"=([^&#]*)";
-  var regex = new RegExp( regexS );
-  var results = regex.exec( window.location.href );
-  if( results == null )
-    return null;
-  else
-    return results[1];
+function value_of_get_param(name) {
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]")
+  var regexS = "[\\?&]"+name+"=([^&#]*)"
+  var regex = new RegExp( regexS )
+  var results = regex.exec( window.location.href )
+  return (results==null) ? null : results[1].replace("+", " ")
 }
